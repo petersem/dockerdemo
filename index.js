f0c1e1107d3bb64c00c594fdd431451dec0667ea
Matthew Petersen <mpetersen@pobox.com>
2025-09-15 17:20:44 +1000

    initial creation

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+var { hostname, networkInterfaces } = require("os");
+var hostname = hostname();
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+        if (net.family === familyV4Value && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+
+            results[name].push(net.address);
+        }
+    }
+}
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/status', (req, res) => {
+    const curTime = new Date();
+    var output = curTime.toLocaleTimeString() + " Hi from " + hostname + " at " + JSON.stringify(results);
+    res.json(output);
+});
+
+app.listen(3210, () => {
+    console.log("Docker demo running on " + hostname + ":3210");
+});
